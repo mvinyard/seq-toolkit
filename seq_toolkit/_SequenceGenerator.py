ecf86465e14fb7cfbc6c3886d9e03272586f26d0
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2021-12-02 08:28:05 -0500

    Update _SequenceGenerator.py

--- seq_toolkit/_SequenceGenerator.py
+++ seq_toolkit/_SequenceGenerator.py
@@ -3,7 +3,22 @@ import numpy as np
 
 def _set_weight_simplex(A=1, C=1, G=1, T=1):
     
-    """"""
+    """
+    Change the composition of weights for sampling bases at random.
+
+    Parameters:
+    ----------
+    N {A, C, G, T}
+        proportions of bases to be sampled. simplex. 
+
+    Returns:
+    -------
+    Initializes self.Gene and creates/modifies self.SeqGen
+
+    Notes:
+    ------
+    (1) As an example, if A=2, the simplex vector would appear as: [0.4, 0.2, 0.2, 0.2]
+    """
     
     bases = np.array([A, C, G, T])
     return bases / bases.sum()
@@ -11,11 +26,58 @@ def _set_weight_simplex(A=1, C=1, G=1, T=1):
 class _SequenceGenerator:
     
     def __init__(self, A=1, C=1, G=1, T=1,):
+
+        """
+        Initialize random sequence generator.
+
+        Parameters:
+        ----------
+        N {A, C, G, T}
+            proportions of bases to be sampled. simplex. 
+
+        Returns:
+        -------
+        self.bases
+            Array of available bases.
+            type: numpy.ndarray
+
+        self.weights
+            Weights to be passed for base selection.
+            type: numpy.ndarray
+
+        Notes:
+        ------
+        (1) As an example, if A=2, the simplex vector would appear as: [0.4, 0.2, 0.2, 0.2]
+        """
         
         self.bases = np.array(["A", "C", "G", "T"])
         self.weights = _set_weight_simplex(A, C, G, T)      
         
     def simulate(self, n_bases, return_seq=True):
+        
+        
+        """"
+        Simulate a DNA sequence of arbitrary length.
+        
+        Parameters:
+        -----------
+        n_bases
+            Number of bases to be selected.
+            
+        return_seq
+            Indicates if the generated sequence should be returned directly. Saved as a subclass object by default.
+            type: bool
+            default: True
+        
+        Returns:
+        --------
+        [ optional ] self.seq
+            Generated sequence of length(n_bases)
+            type: str
+        
+        Notes:
+        ------
+        """
           
         self.seq = "".join(np.random.choice(self.bases, n_bases, p=self.weights))
         
